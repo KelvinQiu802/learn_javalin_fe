0ffa8dd27afc58704234ece9899b06fca4dbe232
KelvinQiu <kelvinqiu802@outlook.com>
2023-04-20 22:53:07 +0800

    feat: query contact

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import axios from 'axios';
+import './App.css';
+import React from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  let [index, setIndex] = React.useState(0);
+  let [contact, setContact] = React.useState({});
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    let result = await axios.get(`http://localhost:7070/api/contacts/${index}`);
+    setContact(result.data);
+  }
+
+  function handleChange(e) {
+    setIndex(e.target.value);
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <form onSubmit={handleSubmit}>
+        <span>Index: </span>
+        <input type='number' onChange={handleChange} value={index} />
+        <button type='submit'>Query</button>
+      </form>
+
+      <h1>{contact.name}</h1>
+      <h2>{contact.phone}</h2>
+    </div>
+  );
 }
 
-export default App
+export default App;
